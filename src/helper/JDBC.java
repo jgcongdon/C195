fc695c2cb6f558a02c6ce798feb8667c978a9d74
Jackson Congdon <jackson.congdon@gmail.com>
2022-01-13 11:42:48 -0600

    Initial commit

---
+++ src/helper/JDBC.java
@@ -0,0 +1,42 @@
+package helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class JDBC {
+
+    private static final String protocol = "jdbc";
+    private static final String vendor = ":mysql:";
+    private static final String location = "//localhost/";
+    private static final String databaseName = "client_schedule";
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER"; // LOCAL
+    private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
+    private static final String userName = "sqlUser"; // Username
+    private static String password = "Passw0rd!"; // Password
+    public static Connection connection;  // Connection Interface
+
+    public static void openConnection()
+    {
+        try {
+            Class.forName(driver); // Locate Driver
+            connection = DriverManager.getConnection(jdbcUrl, userName, password); // Reference Connection object
+            System.out.println("Connection successful!");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+            System.out.println("Connection closed!");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,30 @@
 package main;
 
+import helper.JDBC;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    @Override
+public class Main /*extends Application*/ {
+    /*@Override
     public void start (Stage stage) throws Exception {
+
+
+
         Parent root = FXMLLoader.load(getClass().getResource("/view/FirstScreen.fxml"));
         stage.setTitle("First Screen");
         stage.setScene(new Scene(root, 800,600));
         stage.show();
-    }
+    }*/
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+
+        JDBC.openConnection();
+        JDBC.closeConnection();
     }
 
 }
